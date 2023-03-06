c90d419f4947ff751c2911c2ac30013831cd6d48
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-03-06 22:31:09 +0300

    data: add data to the application

---
+++ src/data/venueData.js
@@ -0,0 +1,58 @@
+export const dreamVenues = [
+  {
+    choice: "A",
+    name: "Garden Greenery",
+    subOptions: [
+      { venueName: "The Japanese Freindship Garden" },
+      { venueName: "The Windmill Winery - Lake House" },
+      { venueName: "Schnep's Farm - Meadow" },
+      { venueName: "Tubac Golf Course and Resort" },
+      { venueName: "Verrado Golf Club" },
+      { venueName: "Encanterra Golf Club" },
+    ],
+  },
+  {
+    choice: "B",
+    name: "Desert drean",
+    subOptions: [{ venueName: "The Paseo" }, { venueName: "Desert Foothills" }],
+  },
+  {
+    choice: "C",
+    name: "Distinctily arizona",
+    subOptions: [
+      { venueName: "Schnep's Farm - Farm House" },
+      { venueName: "Hacienda Del Sol" },
+      { venueName: "Tanque Verde - Saguaro Room" },
+      { venueName: "The Historic Bates Mansion" },
+      { venueName: "The Irish Cultural Center" },
+      { venueName: "The Ocotillo" },
+      { venueName: "The Vic" },
+      { venueName: "La Cocina at Encanterra " },
+      { venueName: "The Rubi House" },
+    ],
+  },
+  {
+    choice: "D",
+    name: "Industrial",
+    subOptions: [{ venueName: "Sunkist" }, { venueName: "Fabric" }],
+  },
+  {
+    choice: "E",
+    name: "Modern",
+    subOptions: [
+      { venueName: "Juncture PHX" },
+      { venueName: "Soho63" },
+      { venueName: "Hotel Valley Ho" },
+    ],
+  },
+  {
+    choice: "F",
+    name: "Rustic yet Chic",
+    subOptions: [
+      { venueName: "The Windmill Winery - Big Red Barn" },
+      { venueName: "Schnep's Farm - Big Red Barn" },
+      { venueName: "Tanque Verde - Big Barn" },
+      { venueName: "Tanque Verde - Cottonwood Grove" },
+    ],
+  },
+];
