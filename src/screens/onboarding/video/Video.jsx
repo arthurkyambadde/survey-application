7f0a67e15af5052a1ec494582af4c6c27b0b046d
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-03-07 19:22:04 +0300

    (refactor): Improve sematic tag usage

--- src/screens/onboarding/video/Video.jsx
+++ src/screens/onboarding/video/Video.jsx
@@ -7,7 +7,7 @@ function VideoIntroduction({ title, video }) {
       className={`h-screen w-screen flex items-center flex-col justify-center`}
     >
       <div className="">
-        <h3 className="text-2xl font-bold mb-8 ">{title}</h3>
+        <h1 className="text-2xl font-bold mb-8 ">{title}</h1>
 
         <video width={"800"} controls className="mb-8">
           <source src={video} type="video/mp4" />
