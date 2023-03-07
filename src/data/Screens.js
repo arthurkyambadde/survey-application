3a09581c884bb4c8d534b9381cf4514689960e7e
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-03-07 19:39:21 +0300

    (refactor): Add data for onboarding screen.
    - Add data that'll be running the onboarding UI.

---
+++ src/data/Screens.js
@@ -0,0 +1,12 @@
+export const ONBOARDING_DATA = {
+  introduction: {
+    title: "The Venue Ranker",
+    video: "../url/to/video.mp4",
+  },
+  instructions: {
+    title: "Pixie point",
+    subtitle: `As you work through the ranker, please only select one as "my favorite."`,
+    text: `Each venue has a "venue specifics" PDF for you to download and review. Be sure to look this over before deciding if a venue is the one for you.`,
+    image: "../url/to/image.jpg",
+  },
+};
