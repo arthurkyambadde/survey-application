e241c6b14337310987cce8f8777faa27224803f9
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-03-08 01:48:52 +0300

    (feat): add Radio component to choice component

--- src/components/choice/Choice.jsx
+++ src/components/choice/Choice.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import tickIcon from "../../assets/icons/tick.svg";
 import CheckBox from "../inputs/CheckBox";
+import Radio from "../inputs/Radio";
 
 function Choice({ type, ...props }) {
   if (type === "multi_select") {
     return <CheckBox {...props} />;
+  }
+
+  if (type === "single_select") {
+    return <Radio {...props} />;
   }
 
   return null;
