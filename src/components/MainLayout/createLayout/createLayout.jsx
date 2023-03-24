a4bf5333e5db042bf7bcbc802aaeaf13342cd4af
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-03-24 11:59:24 +0300

    (feat): create HOC for creating a layout

---
+++ src/components/MainLayout/createLayout/createLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function createLayout(layout, components) {
+  return function () {
+    return (
+      <>
+        {layout.map((item) => {
+          const Component = components[item.type];
+          return <Component {...item} key={item.id} />;
+        })}
+      </>
+    );
+  };
+}
