ccd65d0910f60be7997f66a61c7e1f6c2b65fe08
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-03-25 14:13:04 +0300

    (refactor): remove un-used import

--- src/contexts/SelectedVenuesContext.js
+++ src/contexts/SelectedVenuesContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
 import { INITIAL_PLACES_RANKING } from "../data/data";
 
 export const VenuesContext = createContext();
