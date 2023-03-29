502cedf4895cd8f28c2e9827ccf880e1a6c7a89d
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-03-30 01:53:55 +0300

    (feat): modify source of options

--- src/screens/appointment/AppointmentIntroduction.jsx
+++ src/screens/appointment/AppointmentIntroduction.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Inquiry } from "../../components/question/Inquiry";
+import { useVenue } from "../../hooks/useVenue";
 
 function AppointmentIntroduction(props) {
+  const { layoutData } = useVenue();
+
+  const options = layoutData.find((obj) => obj.id === props.id).questions
+    .question;
+
   return (
     <Inquiry
+      id={props.id}
       type={props.type}
       title={props.introduction}
       subtitle={props.subtitle}
-      options={props.question}
+      options={options}
     />
   );
 }
