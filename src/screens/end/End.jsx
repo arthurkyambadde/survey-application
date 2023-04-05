46981581e6f35b0a3c1ea92a43cd5cfe43d8866f
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-04-05 23:26:38 +0300

    (feat): use form1.json insted of data.json

--- src/screens/end/End.jsx
+++ src/screens/end/End.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { MediaViewer } from "../../components/MediaViewer/MediaViewer";
-import { data } from "../../data/data";
 
-export default function End() {
+export default function End(props) {
+  const layout = props.item.layout;
+
   return (
     <section
       className={`h-screen w-screen flex items-center flex-col justify-center`}
     >
       <div className="h-[349.14px] w-[349.14px] object-cover">
-        <MediaViewer imageUrl={data.end_page.link} />
+        <MediaViewer imageUrl={layout.link} />
       </div>
       <div className="w-1/3 text-center text-4xl sm:w-full sm:p-8">
-        <p>{data.end_page.text}</p>
+        <p>{layout.text}</p>
       </div>
     </section>
   );
